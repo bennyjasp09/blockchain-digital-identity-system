40fea3ee827939516081f629a96170789d6e1a83
bennyjasp09 <168684919+bennyjasp09@users.noreply.github.com>
2024-07-13 23:02:40 -0400

    Update Identity contract to include phone and date of birth fields

--- contracts/Identity.sol
+++ contracts/Identity.sol
@@ -2,13 +2,22 @@
 pragma solidity ^0.8.0;
 
 contract Identity {
-    mapping(address => string) private identities;
-
-    function createIdentity(string memory did) public {
-        identities[msg.sender] = did;
+    struct User {
+        string did;
+        string name;
+        string email;
+        string phone;
+        string dob; // Date of birth
     }
 
-    function getIdentity(address user) public view returns (string memory) {
-        return identities[user];
+    mapping(address => User) private users;
+
+    function createIdentity(string memory did, string memory name, string memory email, string memory phone, string memory dob) public {
+        users[msg.sender] = User(did, name, email, phone, dob);
+    }
+
+    function getIdentity(address user) public view returns (string memory, string memory, string memory, string memory, string memory) {
+        User memory userInfo = users[user];
+        return (userInfo.did, userInfo.name, userInfo.email, userInfo.phone, userInfo.dob);
     }
 }
